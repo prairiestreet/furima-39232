e3b45eff75821c62d9c5210a3c615302a913480a
prairiestreet <engineerkeisuke@gmail.com>
2023-08-24 20:17:35 +0900

    新規登録ができる

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :last_name_full_width, :first_name_full_width, 
+            :last_name_full_width_katakana, :first_name_full_width_katakana, :birthday, presence: true
 end
