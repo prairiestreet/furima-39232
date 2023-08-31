f50218600b9a7d089c153c498136ca416bdbfcd8
prairiestreet <engineerkeisuke@gmail.com>
2023-08-31 18:26:20 +0900

    エラーハンドリングができる

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
 
   validates :nickname, :last_name_full_width, :first_name_full_width, 
             :last_name_full_width_katakana, :first_name_full_width_katakana, :birthday, presence: true
+  validates :last_name_full_width_katakana, :first_name_full_width_katakana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "is invalid. Input full-width katakana characters" }
+  validates :last_name_full_width, :first_name_full_width, format: { with: /\A[\p{hiragana}\p{katakana}\p{blank}ー－]+\z/, message: "is invalid. Input full-width characters" }
+  validate :password_complexity
+
+  private
+
+  def password_complexity
+    return if password.blank? || (password =~ /[a-zA-Z]/ && password =~ /[0-9]/)
+    errors.add :password, 'is invalid. Include both letters and numbers'
+  end
 end
