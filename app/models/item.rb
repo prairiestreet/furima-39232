3e1c96a50ac181585d1ebc472c9f78a1a1b43b88
prairiestreet <engineerkeisuke@gmail.com>
2023-09-05 23:57:44 +0900

    モデルとテーブルを作成
    商品出品情報を保存するためのモデルとテーブルを作成

---
+++ app/models/item.rb
@@ -0,0 +1,10 @@
+class Item < ApplicationRecord
+
+  belongs_to :user
+  has_one :purchase_record
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :prefectures
+  belongs_to_active_hash :preparation_days
+end
