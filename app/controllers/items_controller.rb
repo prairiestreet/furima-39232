d2852080fcc5b4b15d9f21b99e031eb55d018948
prairiestreet <engineerkeisuke@gmail.com>
2023-09-07 16:36:08 +0900

    商品出品ページへ遷移する
     ログイン状態の場合のみ、商品出品ページへ遷移できる。
     ログアウト状態の場合は、商品出品ページへ遷移しようとすると、ログインページへ遷移する。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,30 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  
   def index
+    @items = Item.all.order(created_at: :desc)
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def message_params
     params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :other_attributes)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,14 @@
 class Item < ApplicationRecord
 
   belongs_to :user
-  has_one :purchase_record
-  belongs_to_active_hash :category
-  belongs_to_active_hash :status
-  belongs_to_active_hash :shipping_fee
-  belongs_to_active_hash :prefectures
-  belongs_to_active_hash :preparation_days
+  has_one :order
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :preparation_day
+
 end
