e949c2ff5dcf27e476ea7504cabfaad32727e7d2
prairiestreet <engineerkeisuke@gmail.com>
2023-09-10 22:52:34 +0900

    商品情報編集機能修正2
    Rubocop済み

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :confirm, only: [:edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
