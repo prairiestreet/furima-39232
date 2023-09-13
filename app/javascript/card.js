63006c6f0ca2f2ba627392b601efebaf7a43421c
prairiestreet <engineerkeisuke@gmail.com>
2023-09-14 04:49:51 +0900

    STEP2 手順④

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,7 +1,6 @@
 const pay = () => {
   const payjp = Payjp('pk_test_d5ba8e2b2c77ff2bdbfad92b')
 
-    // 入力フォームの作成
     const elements = payjp.elements();
     const numberElement = elements.create('cardNumber');
     const expiryElement = elements.create('cardExpiry');
@@ -21,8 +20,14 @@ const pay = () => {
       if (response.error) {
       } else {
         const token = response.id;
-        console.log(token)
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
+      numberElement.clear();
+      expiryElement.clear();
+      cvcElement.clear();
+      document.getElementById("charge-form").submit();
     });
   });
 };
