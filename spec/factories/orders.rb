9ee3484025810ec1490f9a32a80036f43f371c37
prairiestreet <engineerkeisuke@gmail.com>
2023-09-14 01:41:54 +0900

    STEP1手順5と6

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
-  factory :purchase_shipping do
+  factory :order do
     postal_code    { '123-4567' }
-    prefecture_id  { '48' }
+    prefecture     { '48' }
     city           { 'うるま市' }
     address        { 'みどり町1-1-1' }
     building_name  { '東棟' }
