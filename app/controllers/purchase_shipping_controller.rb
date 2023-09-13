dbde656bb5547b10415631881cb79d77bc2ddf31
prairiestreet <engineerkeisuke@gmail.com>
2023-09-13 09:16:44 +0900

    STEP1手順2
    購入機能に必要なモデルやコントローラーを作成する

--- app/controllers/purchase_shipping_controller.rb
+++ app/controllers/purchase_shipping_controller.rb
@@ -2,6 +2,7 @@ class PurchaseShippingController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @item = Item.find(params[:item_id]) # @item を定義する
     @purchase_shipping = PurchaseShipping.new
   end
 
@@ -9,10 +10,21 @@ class PurchaseShippingController < ApplicationController
   end
 
   def create
-    @purchase_shipping = PurchaseShipping.new(purchase_record_params)
+    @item = Item.find(params[:item_id]) # @item を定義する
+    @purchase_shipping = PurchaseShipping.new(purchase_shipping_params)
     if @purchase_shipping.valid?
       pay_item
-      @purchase_shipping.save
+      # PurchaseRecord を作成し、データベースに保存
+      purchase_record = PurchaseRecord.create(
+        user_id: current_user.id,
+        item_id: @item.id
+      )
+
+      # ShippingArea を作成し、データベースに保存
+      shipping_area = ShippingArea.create(
+        # 必要な発送先情報を設定
+      )
+
       return redirect_to root_path
     else
       render :index
@@ -22,10 +34,12 @@ class PurchaseShippingController < ApplicationController
 
   private
 
-  def purchase_record_params
+  def purchase_shipping_params
     params.require(:purchase_shipping).permit(:postal_code, :prefecture, :city, :address, :building_name,
                                           :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 
-
+  def pay_item
+    # 支払い処理を実装する（実際の支払いゲートウェイを使用するか、テスト用の処理を記述）
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root to: "items#index"
   resources :users, only: [:new, :create]
   resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
+# 購入に関連するルートを追加
+    resources :purchase_shipping, only: [:index, :new, :create], controller: 'purchase_shipping'
     resources :purchase_records, only: [:index, :new, :create]
   end
-  get 'items/new', to: 'items#new' 
-end
+  end
